78c06af4e714ac00deacf3bd808a65b4069b5a13
Adam Stepanczuk <Stepanczuk98@gmail.com>
2021-11-13 12:34:15 +0100

    Create DB

---
+++ ShopProject/database/migrations/2021_11_13_104120_create_orders.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('orders');
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            //$table->integer('user_id');
+            $table->decimal('total_price');
+            $table->timestamps();
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable()->constrained()->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
